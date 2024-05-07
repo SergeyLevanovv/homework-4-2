01195a7eed62e3649f0ac455b1f275c0b1817b29
SergeyLevanovv <ser.lewanow@gmail.com>
2024-05-07 04:02:14 +0400

    add Task-4 explanation

---
+++ src/Task4.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество строк ромба: ");
+        int rows = scanner.nextInt();
+
+        System.out.println("Заполненный ромб:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = rows - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = rows - 2; i >= 0; i--) {
+            for (int j = rows - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nПустой ромб:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = rows - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                if (k == 0 || k == i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = rows - 2; i >= 0; i--) {
+            for (int j = rows - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                if (k == 0 || k == i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
